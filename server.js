9b6678fe3e0f10bc05b0160924f161b38f5cb708
George Gebbett <george.gebbett1@gmail.com>
2022-01-14 14:18:47 +0000

    iahsfjbnsdf

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors")
+const path = require("path");
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+const puzzleNumber = 0;
+const todaysWord = "MANGE";
+
+
+app.use(express.json());
+app.use(express.static('build'));
+
+app.get("/api/word", ((req, res) => {
+    res.json({puzzleNumber, todaysWord})
+}))
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
